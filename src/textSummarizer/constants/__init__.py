766abd39abce539e4be2010a8e2179e931597297
Tejaswi Ganti <tejaswi.gln@gmail.com>
2024-04-05 14:52:38 +0530

    changing param.yaml path

--- src/textSummarizer/constants/__init__.py
+++ src/textSummarizer/constants/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path 
-
-CONFIG_FILE_PATH = Path("config/config.yaml")
-PARAMS_FILE_PATH = Path("params.yaml")
+CONFIG_FILE_PATH = Path("D:/Code/Text-Summarizer/config/config.yaml")
+# CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("D:/Code/Text-Summarizer/params.yaml")
