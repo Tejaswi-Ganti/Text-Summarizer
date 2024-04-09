8398f34a97fc9cdf99b40d96151c432171a2eb73
Tejaswi Ganti <tejaswi.gln@gmail.com>
2024-04-10 01:00:53 +0530

    deploying data_tranformation code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.textSummarizer.pipeline.stage_01_dataingestion import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.stage_02_datavalidation import DataValidationTrainingPipeline
+from src.textSummarizer.pipeline.stage_03_datatransformation import DataTransformationTrainingPipeline
 from src.textSummarizer.logging import logger
 
 
@@ -20,6 +21,16 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_validation = DataValidationTrainingPipeline()
    data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
